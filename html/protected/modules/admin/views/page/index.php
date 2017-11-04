6467aa1f0f9b7c1046bb4774dfe1498b730e6c0f
ptflp <filippov.pa@s-vfu.ru>
2017-11-05 00:23:52 +0900

    editing page/index view

--- html/protected/modules/admin/views/page/index.php
+++ html/protected/modules/admin/views/page/index.php
@@ -38,12 +38,23 @@ $('.search-form form').submit(function(){
 	'columns'=>array(
 		'id',
 		'title',
-		'content',
-		'created',
-		'status',
-		'category_id',
+		'created'=>array(
+			'name' => 'created',
+			'value' => 'date("j.m.Y H:i", $data->created)'
+		),
+		'status' => array(
+			'name' => 'status',
+			'value' => '($data->status==1)?"Доступно":"Скрыто"',
+			'filter' => array(0=>'Скрыто', 1=>'Доступно')
+		),
+		'category_id'=> array(
+			'name' => 'category_id',
+			'value' => '$data->category->title',
+			'filter' => Category::all()
+		),
 		array(
 			'class'=>'CButtonColumn',
+			'viewButtonOptions' => array('style' => 'display: none;')
 		),
 	),
 )); ?>
