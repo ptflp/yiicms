8433793a09aef3ac41ca8d6efb0c94268052e704
ptflp <filippov.pa@s-vfu.ru>
2017-11-06 23:18:19 +0900

    removed right option menu

--- html/protected/modules/admin/views/comment/view.php
+++ html/protected/modules/admin/views/comment/view.php
@@ -8,11 +8,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Comment', 'url'=>array('index')),
-	array('label'=>'Create Comment', 'url'=>array('create')),
-	array('label'=>'Update Comment', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Comment', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Comment', 'url'=>array('admin')),
+	array('label'=>'Журнал комментариев', 'url'=>array('index')),
+	array('label'=>'Delete Comment', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Вы уверены?')),
 );
 ?>
 
