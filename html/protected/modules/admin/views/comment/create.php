9f7ae3292a1203632c1bb40d0962c80309493da1
ptflp <filippov.pa@s-vfu.ru>
2017-11-06 21:02:44 +0900

    removing breadcrumb

--- html/protected/modules/admin/views/comment/create.php
+++ html/protected/modules/admin/views/comment/create.php
@@ -2,11 +2,6 @@
 /* @var $this CommentController */
 /* @var $model Comment */
 
-$this->breadcrumbs=array(
-	'Comments'=>array('index'),
-	'Create',
-);
-
 $this->menu=array(
 	array('label'=>'Журнал Comment', 'url'=>array('index')),
 	array('label'=>'Manage Comment', 'url'=>array('admin')),
