b37325c2e40e7d3a51c548f0eb4aaefca768b620
ptflp <filippov.pa@s-vfu.ru>
2017-11-06 23:59:46 +0900

    created form

---
+++ html/protected/modules/admin/views/setting/index.php
@@ -0,0 +1,40 @@
+<h1>Настройки</h1>
+
+<?php
+/* @var $this CategoryController */
+/* @var $model Category */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'setting-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'defaultStatusComment'); ?>
+		<?php echo $form->checkBox($model,'defaultStatusComment',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'defaultStatusComment'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'defaultStatusUser'); ?>
+		<?php echo $form->checkBox($model,'defaultStatusUser'); ?>
+		<?php echo $form->error($model,'defaultStatusUser'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Сохранить'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
