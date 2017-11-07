15bcdeb2432bb9c7242d22f1925054a7dc694a7d
ptflp <filippov.pa@s-vfu.ru>
2017-11-07 23:15:38 +0900

    added title of news item (page)

--- html/protected/views/page/view.php
+++ html/protected/views/page/view.php
@@ -2,6 +2,6 @@
 /* @var $this PageController */
 
 $this->breadcrumbs=array(
-	'Новости - '.$model->category->title => array('index', 'id' => $model->category_id),
+	'Новости - '.$model->category->title => array('index', 'id' => $model->category_id),$model->title
 );
 ?>
