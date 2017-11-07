0d78d198cdec7017734d3c7103099a1a27c5e2bc
ptflp <filippov.pa@s-vfu.ru>
2017-11-07 20:23:17 +0900

    layout modification

--- html/protected/views/layouts/column2.php
+++ html/protected/views/layouts/column2.php
@@ -1,10 +1,5 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
-</div>
 <div class="span-5 last">
 	<div id="sidebar">
 	<?php
@@ -18,5 +13,10 @@
 		$this->endWidget();
 	?>
 	</div><!-- sidebar -->
+</div>
+<div class="span-19">
+	<div id="content">
+		<?php echo $content; ?>
+	</div><!-- content -->
 </div>
 <?php $this->endContent(); ?>
