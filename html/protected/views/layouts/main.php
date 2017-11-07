ee32627818d34ad8b9fa239bd2fc8f84841a8e12
ptflp <filippov.pa@s-vfu.ru>
2017-11-07 20:50:27 +0900

    menu layout from db

--- html/protected/views/layouts/main.php
+++ html/protected/views/layouts/main.php
@@ -28,14 +28,7 @@
 
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Админка', 'url'=>array('/admin')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
+			'items'=>Category::menu('top'),
 		)); ?>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
