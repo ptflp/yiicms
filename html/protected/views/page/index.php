25bda39ccb4056879c0a9dff787a09ecb71fef86
ptflp <filippov.pa@s-vfu.ru>
2017-11-07 22:28:09 +0900

    mking link by CHtml::link

--- html/protected/views/page/index.php
+++ html/protected/views/page/index.php
@@ -7,8 +7,10 @@ $this->breadcrumbs=array(
 ?>
 <?php
 foreach ($models as $one) {
- 	echo '<h3>',$one->title,'</h3>';
+ 	echo CHtml::link('<h3>'.$one->title.'</h3>',array('view','id'=>$one->id));
  	echo substr($one->content,0,260);
+ 	echo CHtml::link('Читать далее...',array('view','id'=>$one->id));
+ 	echo ('<br><br><hr/>');
  } 
 
 if (!$models) {
