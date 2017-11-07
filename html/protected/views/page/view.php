148a7395ddd1b4ba27f108ae9e898c0741a8db1d
ptflp <filippov.pa@s-vfu.ru>
2017-11-07 23:18:30 +0900

    showing title date content

--- html/protected/views/page/view.php
+++ html/protected/views/page/view.php
@@ -5,3 +5,13 @@ $this->breadcrumbs=array(
 	'Новости - '.$model->category->title => array('index', 'id' => $model->category_id),$model->title
 );
 ?>
+<h1>
+<?php 
+	echo $model->title;
+ ?>
+</h1>
+<?php echo date('j.m.Y H:i',$model->created); ?>
+<hr>
+ <?php 
+ 	echo $model->content;
+  ?>
