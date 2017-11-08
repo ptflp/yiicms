1373c95e427a1b634923da66b744056425828577
ptflp <filippov.pa@s-vfu.ru>
2017-11-08 23:51:50 +0900

    registration page created via VC

--- html/protected/controllers/SiteController.php
+++ html/protected/controllers/SiteController.php
@@ -39,7 +39,7 @@ class SiteController extends Controller
 				$this->redirect(array('index','id'=>$model->id));
 		}
 
-		$this->render('create',array(
+		$this->render('registration',array(
 			'model'=>$model,
 		));
 	}
